8f8668d9f996c92f7d241f058b80e152f1c34e93
Supun Priyanath <www.supunbro12@gmail.com>
2026-01-20 22:31:47 +0530

    Upadate humberg menu

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BottomTabs from "./BottomTabs";
+import HamburgerMenu from "../components/HamburgerMenu";
 
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Tabs" component={BottomTabs} />
+    <Stack.Navigator
+      screenOptions={({ navigation }) => ({
+        headerTitleAlign: "center",
+        headerRight: () => (
+          <HamburgerMenu onPress={() => navigation.openDrawer?.()} />
+        ),
+      })}
+    >
+      <Stack.Screen
+        name="MainTabs"
+        component={BottomTabs}
+        options={{ headerTitle: "UniSpace" }}
+      />
     </Stack.Navigator>
   );
 }
