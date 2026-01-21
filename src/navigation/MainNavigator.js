09e791d6d8be5c5b176effe937b42fec03ebd210
Supun Priyanath <www.supunbro12@gmail.com>
2026-01-21 22:08:13 +0530

    update Hamberg menu

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,24 +1,15 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BottomTabs from "./BottomTabs";
-import HamburgerMenu from "../components/HamburgerMenu";
 
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
   return (
-    <Stack.Navigator
-      screenOptions={({ navigation }) => ({
-        headerTitleAlign: "center",
-        headerRight: () => (
-          <HamburgerMenu onPress={() => navigation.openDrawer?.()} />
-        ),
-      })}
-    >
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name="MainTabs"
         component={BottomTabs}
-        options={{ headerTitle: "UniSpace" }}
       />
     </Stack.Navigator>
   );
